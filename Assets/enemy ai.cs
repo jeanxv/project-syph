07c947d63d7cdced51726e2d325222b1d96a9ce8
jeanxv <145032317+jeanxv@users.noreply.github.com>
2024-01-25 12:47:34 +0000

    ykxkngh,

--- Assets/enemy ai.cs
+++ Assets/enemy ai.cs
@@ -7,6 +7,9 @@ public class EnemyAi : MonoBehaviour
 {
 
     [SerializeField]Transform target;
+    [SerializeField]
+    private float _playerAwarenessDistance;
+
 
     NavMeshAgent agent;
 
@@ -16,11 +19,23 @@ public class EnemyAi : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+
+        Player = GameObject.Find("Player");
     }
+
+
+
+
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(target.position);
+
+        gameObject.GetComponent<NavMeshAgent>().enabled = false;
+        if ((player.transform.position - this.transform.position).sqrMagnitude < 3 * 3) ;
+        {
+            gameObject.GetComponent<NavMeshAgent>().enabled = true;
+        }
     }
 }
--- Assets/player notice.cs
+++ Assets/player notice.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerAwarenessController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool AwareOfPlayer { get; private set; }
+
+    public Vector2 DirectionToPlayer { get; private set; }
+
+    [SerializeField]
+    private float _playerAwarenessDistance;
+
+    private Transform _player;
+
+    private void Awake()
     {
-        
+        _player = FindObjectOfType<PlayerAwarenessController>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 enemyToPlayerVector = _player.position - transform.position;
+        DirectionToPlayer = enemyToPlayerVector.normalized;
+
+        if (enemyToPlayerVector.magnitude <= _playerAwarenessDistance)
+        {
+            AwareOfPlayer = true;
+        }
+        else
+        {
+            AwareOfPlayer = false;
+        }
     }
 }
