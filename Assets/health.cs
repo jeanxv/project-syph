0d4cd193d288197f37c99651629e26859e4a8fd2
jeanxv <145032317+jeanxv@users.noreply.github.com>
2024-01-29 16:40:06 +0000

    f\bfb

---
+++ Assets/health.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+
+
+{
+
+    public int MaxHealth = 0;
+    public int currentHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth -= 3;
+    }
+     public void TakeDamage (int amaount)
+    {
+        currentHealth = amaount;
+
+        if (currentHealth <= 0)
+        {
+         // dead 
+        }
+    }
+    // Update is called once per frame
+    public void Heal(int amaount) 
+    {
+       currentHealth += amaount;
+        if (currentHealth > MaxHealth)
+        {
+            currentHealth = MaxHealth;
+        }
+    }
+}
